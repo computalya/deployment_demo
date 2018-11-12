7c9fa2815271fa5d2944c7370790e71ddf1244fe
Atilla GÜNDÜZ <gunduz.atilla@gmail.com>
2018-11-12 19:23:06 +0300

    add linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :linked_files, %w{config/master.key}
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
