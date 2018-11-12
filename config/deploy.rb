670d3d3517600046fb2792f7064fe433e7af8166
Atilla GÜNDÜZ <gunduz.atilla@gmail.com>
2018-11-12 22:37:46 +0300

    update Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,7 @@ set :linked_files, %w{config/master.key}
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
