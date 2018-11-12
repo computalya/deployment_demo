e6e703bce0dbc5ae476db51a6525d16186ed06d4
Atilla GÜNDÜZ <gunduz.atilla@gmail.com>
2018-11-12 23:32:34 +0300

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,8 @@ set :stage,           :production
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :deploy_to, "/var/www/deployment_demo"
+# set :deploy_to, "/var/www/deployment_demo"
+set :deploy_to, "/var/www/#{fetch :application}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
